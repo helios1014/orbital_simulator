819b40ec474715c8d8ba02a7589525af341c4126
helios1014 <helios1014@gmail.com>
2025-12-18 22:54:23 -0500

    first

---
+++ c/ball.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <SDL3/SDL.h>
+
+#define WIDTH 900
+#define HEIGHT 600
+//window does not open in wayland compositor, well hyprland for me 
+//
+int main()
+{
+  printf("ball pit open\n");
+  SDL_Init(SDL_INIT_VIDEO); //clearify
+  SDL_CreateWindow("ball pit", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+  SDL_Delay(3000);
+
+}
---
+++ sdl_notes.md
@@ -0,0 +1,5 @@
+https://www.reddit.com/r/sdl/comments/1mfqdic/wrapping_sdl_while_using_sdl_main_functions/
+https://discourse.libsdl.org/t/sdl3-any-reason-to-prefer-main-or-callbacks/57286
+https://github.com/libsdl-org/SDL/blob/main/docs/README-main-functions.md
+https://examples.libsdl.org/SDL3/demo/03-infinite-monkeys/
+https://www.youtube.com/watch?v=27iEILBpI04
